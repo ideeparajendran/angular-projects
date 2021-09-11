ab6fbfbb4c079e9ac575a2cb2614e435163a35c6
ideeparajendran <90501513+ideeparajendran@users.noreply.github.com>
2021-09-12 01:26:14 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, VERSION } from '@angular/core';
+import { AppService } from './app.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +9,30 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent {
   cities: string[] = ['Raleigh', 'Chicago', 'Austin', 'Seattle', 'LosAngeles'];
+  date: string;
+  convertedDate: string;
+  day: string;
+  dateop: string;
+  temp: string;
+  constructor(private appService: AppService, private datePipe: DatePipe) {}
+
+  onClick() {
+    this.appService.fetchData().subscribe(res => {
+      console.log(res);
+
+      this.date = res['data'][0]['datetime'];
+      this.convertedDate = this.datePipe.transform(this.date, 'fullDate');
+
+      var dateData = this.convertedDate.split(',');
+      this.day = dateData[0];
+      this.dateop = dateData[1];
+
+      // temp
+      this.temp = res['data'][0]['temp'] + '°C';
+
+      //icons
+      this.date = res['data'][0]['weather']['icon'];
+
+    });
+  }
 }
